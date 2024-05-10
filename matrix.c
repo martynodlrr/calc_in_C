6b60cd3c3c59b62a196332a6262d9b650d50d15d
Martyn <martynodlrr@gmail.com>
2024-05-10 18:46:01 -0500

    this was pushed with a bash alias command

--- matrix.c
+++ matrix.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
-#include <ncurses.h>
+
+#include <windows.h>
 
 #define WIDTH  80
 #define HEIGHT 40
@@ -8,60 +9,75 @@
 #define idx(x, y) ((x)+WIDTH*(y))
 
 struct trail {
-    float start;
-    float end;
-    float speed;
+	float start;
+	float end;
+	float speed;
 } trails[WIDTH];
 
 void init_trail(struct trail *trail)
 {
-    trail->start = -(float)(rand() % HEIGHT);
-    trail->end = 0;
-    trail->speed = (rand() % 5) / 10.0f + 0.2f;
+	trail->start = -(float)(rand() % HEIGHT);
+	trail->end = 0;
+	trail->speed = (rand() % 5) / 10.0f + 0.2f;
 }
 
-void update()
+void update(CHAR_INFO consoleBuffer[])
 {
-    for (int x = 0; x < WIDTH; x++) {
-        int start = trails[x].start < 0 ? 0 : (int)trails[x].start;
-        int end = trails[x].end > HEIGHT ? HEIGHT : (int)trails[x].end;
+	CHAR_INFO space = {.Char.AsciiChar=' ', .Attributes=0};
 
-        for (int y = start; y < end; y++) {
-            move(y, x);
-            if (y < (int)trails[x].end - 1)
-                addch(rand() % 256 | COLOR_PAIR(1));
-            else
-                addch(rand() % 256 | COLOR_PAIR(2));
-        }
+	for (int x = 0; x < WIDTH; x++) {
+		for (int y = 0; y < HEIGHT; y++)
+			consoleBuffer[idx(x, y)] = space;
 
-        trails[x].start += trails[x].speed;
-        trails[x].end += trails[x].speed;
+		int start = trails[x].start < 0 ? 0 : (int)trails[x].start;
+		int end = trails[x].end > HEIGHT ? HEIGHT : (int)trails[x].end;
 
-        if (trails[x].start >= HEIGHT)
-            init_trail(&trails[x]);
-    }
+		for (int y = start; y < end; y++)
+			consoleBuffer[idx(x, y)] = (CHAR_INFO) {
+			.Char.AsciiChar = rand() % 256,
+			.Attributes = FOREGROUND_GREEN |
+			(y < (int)trails[x].end-1 ? 0 :
+			FOREGROUND_RED | FOREGROUND_BLUE)};
+
+		trails[x].start += trails[x].speed;
+		trails[x].end += trails[x].speed;
+
+		if (trails[x].start >= HEIGHT)
+			init_trail(&trails[x]);
+	}
 }
 
 int main(void)
 {
-    srand((unsigned)time(NULL));
+	srand((unsigned)time(NULL));
 
-    initscr();
-    start_color();
-    init_pair(1, COLOR_GREEN, COLOR_BLACK);
-    init_pair(2, COLOR_YELLOW, COLOR_BLACK);
-    curs_set(0);
+	/* Console window */
+	HANDLE wHnd = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    for (int i = 0; i < WIDTH; i++)
-        init_trail(&trails[i]);
+	SMALL_RECT windowSize = {0, 0, WIDTH-1, HEIGHT-1};
+	SetConsoleWindowInfo(wHnd, TRUE, &windowSize);
 
-    for (;;) {
-        update();
-        refresh();
-        napms(16);
-    }
+	COORD bufferSize = {WIDTH, HEIGHT};
+	SetConsoleScreenBufferSize(wHnd, bufferSize);
 
-    endwin();
+	CHAR_INFO consoleBuffer[WIDTH * HEIGHT];
+	COORD charBufSize = {WIDTH, HEIGHT};
+	COORD characterPos = {0, 0};
+	SMALL_RECT writeArea = {0, 0, WIDTH-1, HEIGHT-1};
 
-    return 0;
+	SetConsoleTitle(TEXT("Enter the Matrix"));
+
+	/* Trails */
+	for (int i = 0; i < WIDTH; i++)
+		init_trail(&trails[i]);
+
+	/* Matrix */
+	for (;;) {
+		update(consoleBuffer);
+		WriteConsoleOutputA(wHnd, consoleBuffer,
+				charBufSize, characterPos, &writeArea);
+		Sleep(16);
+	}
+
+	return 0;
 }
